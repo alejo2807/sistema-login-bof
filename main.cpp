abb08d399c446f4c907468f81780d130977e10b6
David <davidgarciag2807@gmail.com>
2025-05-29 11:33:49 -0500

    Archivo main subido para su desarrollo posterior

---
+++ main.cpp
@@ -0,0 +1,7 @@
+// formato del archivo users.txt:
+//nombreCompleto,nombreUsuario,hashContrasena (mini base de datos)
+
+
+
+//SE HACE UNA REESTRUCTURACION DEL CODIGO DE TODO EL PROYECTO:
+//ahora hay 2 clases, SistemaLogin y Usuario, y se implementa un sistema de login básico.
