55154136767886ed04a23f763a9e554d2922a0ee
David <davidgarciag2807@gmail.com>
2025-05-30 19:59:47 -0500

    Implementacion del metodo parseFile()
    Sirve para parsear la base de datos y extraer cada dato como token por separado.
    El token se guarda en la variable correspondiente, ya sea nombreCompletoBaseDatos, etc.

--- SistemaLogin.cpp
+++ SistemaLogin.cpp
@@ -3,54 +3,116 @@
 #include <string>
 #include <map> 
 #include <fstream> // Para manejar archivos, usamos ifstream y ofstream
+#include <sstream> // Para usar stringstream
 using namespace std;
 
 bool SistemaLogin::verificarArchivo()
 {
-    std::ifstream miArchivo("users.txt");
-    if (miArchivo.is_open()) 
-    { 
-        miArchivo.close(); // Cerrar el archivo después de verificar
-        return true; // El archivo existe
-    }
-    return false;
+	std::ifstream miArchivo("users.txt");
+	if (miArchivo.is_open()) 
+	{ 
+		miArchivo.close(); // Cerrar el archivo después de verificar
+		return true; // El archivo existe
+	}
+	return false;
 }
 
 bool SistemaLogin::crearArchivo()
 {
-    if(verificarArchivo()) {
-        return true; // si ya existe el archivo, no es necesario crearlo
-    }
+	if(verificarArchivo()) {
+		return true; // si ya existe el archivo, no es necesario crearlo
+	}
 
-    // Si el archivo no existe, lo creamos
-    std::ofstream nuevoArchivo("users.txt");
-    if (!nuevoArchivo.is_open()) {
-        //vamos a dar mas informacion al usuario si no se pudo crear el archivo
-        std::cerr<< "Error al crear el archivo users.txt.\n Asegúrate de tener permisos de escritura en el directorio." << std::endl;
-        return false; // Si no se pudo abrir el archivo, retornar false
+	// Si el archivo no existe, lo creamos
+	std::ofstream nuevoArchivo("users.txt");
+	if (!nuevoArchivo.is_open()) {
+		//vamos a dar mas informacion al usuario si no se pudo crear el archivo
+		std::cerr<< "Error al crear el archivo users.txt.\n Asegúrate de tener permisos de escritura en el directorio." << std::endl;
+		return false; // Si no se pudo abrir el archivo, retornar false
 
-    }
+	}
 
-    nuevoArchivo.close(); // Cerrar el archivo después de crearlo
-    cout << "Base de datos creada correctamente." << endl;
-    return true; // Archivo creado exitosamente
-    
+	nuevoArchivo.close(); // Cerrar el archivo después de crearlo
+	cout << "Base de datos creada correctamente." << endl;
+	return true; // Archivo creado exitosamente
+	
 }
 
+void SistemaLogin::parseFile()
+{
+	//TRABAJE LA FUNCION PARSE FILE, QUE SE ENCARGA DE LEER EL ARCHIVO Y PARSEARLO.
+	//SUBIR COMMITS DE ESTO, Y LUEGO SEGUIR CON EL PROYECTO.
+	//EN SISTEMA LOGIN SOLO AGREGUE LA FUNCION PERO NO HICE NADA CON ELLA AUN.
+	//HARE COMMIT DE ESO TAMBIEN
+	//DESPUES DE PARSEAR EL ARCHIVO, Y EXTRAER LOS TOKENS, DEBO ALMACENARLOS EN UNA ESTRUCTURA DE DATOS
+	//USARE MAP PARA PRACTICAR PUNTEROS TMB, Y PODER USARLOS MAS ADELANTE.
+	//lo comento porque ya esta creado el archivo users.txt
+	//std::ofstream nuevoArchivo("users.txt");
+	//nuevoArchivo.close();
+	
+	// Intentamos abrir el archivo para leer
+	std::ifstream miArchivo("users.txt");
+	if(miArchivo.is_open()) 
+	{
+		cout << "El archivo se ha abierto correctamente." << endl;
+		string line;
+		
+		// Leemos el archivo línea por línea
+		// Usamos getline para leer cada línea del archivo
+		// Esto nos permite procesar cada línea de manera individual.
+		while(std::getline(miArchivo, line))
+		{	
+			if(line.empty() || line.find("/") == 0) {
+				continue; // Saltar líneas vacías o comentarios
+			}
+			//creamos un stringstream a partir de la línea leída (es como el dispensador de dulces del cual extraemos los dulces (datos))
+			// Esto nos permite procesar la línea de manera más flexible.
+			// Por ejemplo, como la línea contiene datos separados por comas, podemos usar stringstream para extraerlos.
+			std::stringstream stream(line);
+			//los tokens son las partes de la línea que están separadas por comas
+			std::string token;
+			//variables para almacenar los datos (tokens) extraídos de la línea
+			string nombreCompletoBaseDatos, nombreUsuarioBaseDatos, hashContrasenaBaseDatos;
+
+			size_t index = 0; //(lo usaremos para saber en qué parte de la línea estamos)
+			while(std::getline(stream, token, ',')) {
+				if (index == 0) {
+					nombreCompletoBaseDatos = token; // Guardar el nombre completo
+				} else if (index == 1) {
+					nombreUsuarioBaseDatos = token; // Guardar el nombre de usuario
+				} else if (index == 2) {
+					hashContrasenaBaseDatos = token; // Guardar el hash de la contraseña
+				}
+				index++; // Incrementar el índice para la siguiente iteración
+			}
+			cout<<nombreCompletoBaseDatos << " | " 
+					<< nombreUsuarioBaseDatos << " | " 
+					<< hashContrasenaBaseDatos << endl; // Imprimir los datos extraídos
+		}
+		miArchivo.close(); // Cerrar el archivo después de leer		
+	} 
+	else {
+		cout << "Error al abrir el archivo." << endl;
+		return; 
+	}
+	
+	
+	
+}
 
 ////////////////////////////////////////////////////////////////
 
 
 SistemaLogin::SistemaLogin()
 {
-    if(verificarArchivo()) {cout << "Archivo de usuarios encontrado." << endl;} 
+	if(verificarArchivo()) {cout << "Archivo de usuarios encontrado." << endl;} 
 
-    else {
-        cout << "Archivo de usuarios no encontrado. Creando uno nuevo..." << endl;
-        crearArchivo();
-    }
+	else {
+		cout << "Archivo de usuarios no encontrado. Creando uno nuevo..." << endl;
+		crearArchivo();
+	}
 
-    cargarUsuariosDesdeArchivo(); // Cargar usuarios desde el archivo al iniciar el sistema
+	cargarUsuariosDesdeArchivo(); // Cargar usuarios desde el archivo al iniciar el sistema
 }
 
 
--- SistemaLogin.h
+++ SistemaLogin.h
@@ -11,33 +11,34 @@ using namespace std;
 
 class SistemaLogin {
 
-    private:
+	private:
 
-        // Atributos (usaremos un map para almacenar usuarios y hashes de contraseñas)
-        //Ademas, la idea es practicar con los maps, porque nunca los he usado.
-        map<string, Usuario*> usuarios; // usamos un punter a Usuario para  no tener que copiar todo el objeto Usuario
-        Usuario* usuarioActivo; // Puntero al usuario actualmente conectado
+		// Atributos (usaremos un map para almacenar usuarios y hashes de contraseñas)
+		//Ademas, la idea es practicar con los maps, porque nunca los he usado.
+		map<string, Usuario*> usuarios; // usamos un punter a Usuario para  no tener que copiar todo el objeto Usuario
+		Usuario* usuarioActivo; // Puntero al usuario actualmente conectado
 
-        //funciones privadas. No nos interesa que sean accesibles desde fuera de la clase.
-        bool verificarArchivo(); //verifica si el archivo de usuarios existe. TRUE si existe, FALSE si no existe.
-        bool crearArchivo(); //crea el archivo de usuarios si no existe
+		//funciones privadas. No nos interesa que sean accesibles desde fuera de la clase.
+		bool verificarArchivo(); //verifica si el archivo de usuarios existe. TRUE si existe, FALSE si no existe.
+		bool crearArchivo(); //crea el archivo de usuarios si no existe
 
-        bool cargarUsuariosDesdeArchivo(); 
-        void guardarUsuarioEnArchivo(const Usuario& usuario);
-        string hashContrasena(string pass); //retornara la contraseña hasheada
+		void parseFile(); //parses the file to load users into the map
+		bool cargarUsuariosDesdeArchivo(); 
+		void guardarUsuarioEnArchivo(const Usuario& usuario);
+		string hashContrasena(string pass); //retornara la contraseña hasheada
 
-    public:
-        SistemaLogin();
-        ~SistemaLogin();
-        
-        //funciones 
-        bool registrarUsuario(const string& user, const string& pass);
-        bool iniciarSesion();
-        bool verificarCredenciales(const string& user, const string& pass) const;
-        bool cerrarSesion();
-        bool estaConectado(const bool& estaActivo) const;
-        bool cambiarContrasena(const string& oldPass, const string& newPass);
-        bool restablecerContrasena(const string& user, const string& newPass);
+	public:
+		SistemaLogin();
+		~SistemaLogin();
+		
+		//funciones 
+		bool registrarUsuario(const string& user, const string& pass);
+		bool iniciarSesion();
+		bool verificarCredenciales(const string& user, const string& pass) const;
+		bool cerrarSesion();
+		bool estaConectado(const bool& estaActivo) const;
+		bool cambiarContrasena(const string& oldPass, const string& newPass);
+		bool restablecerContrasena(const string& user, const string& newPass);
 
 };
 
