321a27c1fb0c1ceb8b165096b77cf5714533d756
David <davidgarciag2807@gmail.com>
2025-05-30 20:04:01 -0500

    Pequeno update del main

--- main.cpp
+++ main.cpp
@@ -5,3 +5,20 @@
 
 //SE HACE UNA REESTRUCTURACION DEL CODIGO DE TODO EL PROYECTO:
 //ahora hay 2 clases, SistemaLogin y Usuario, y se implementa un sistema de login básico.
+
+
+//vamos a hacer un intento de leer y parsear el archivo de usuarios
+
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <sstream> //to use stringstream
+#include <vector>
+#include <map> //para usar map
+using namespace std;
+
+
+int main()
+{
+
+}
